60266154f12f32d7ac0aec770ced771f93f82cd0
ohaeri ezinne <ohaeriezinne7@gmail.com>
2025-07-11 17:02:19 +0800

    first commit

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,3 +1,8 @@
-<footer>
-  <p>© 2023 City Lovers</p>
-</footer>;
+function footer() {
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+export default footer;
--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,8 +1,8 @@
-function main() {
+function MainContent() {
   return (
     <main>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
-export default main;
+export default MainContent;
