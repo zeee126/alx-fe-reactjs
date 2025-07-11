244d009aadb359c999ae71b0959a198f2f9a147f
ohaeri ezinne <ohaeriezinne7@gmail.com>
2025-07-11 16:55:57 +0800

    first commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <WelcomeMessage />
-    </div>
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
   );
 }
 
