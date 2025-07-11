e73c14ecca02ff2790fbe83b8a1133cd5d25dbbe
ohaeri ezinne <ohaeriezinne7@gmail.com>
2025-07-11 17:09:08 +0800

    app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,11 @@
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import React from "react";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
   );
 }
 
