4643346fbabe29e28339299c7dfb30c73e3769ef
ohaeri ezinne <ohaeriezinne7@gmail.com>
2025-07-22 20:52:39 +0800

    Initial setup with Zustand store and recipe components

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+function App() {
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,19 @@
+import { useRecipeStore } from "../store/recipeStore";
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
