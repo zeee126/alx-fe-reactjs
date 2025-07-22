8bca9baa7f8b1fc58b1143dc43789666ea1c6ffd
ohaeri ezinne <ohaeriezinne7@gmail.com>
2025-07-22 20:13:16 +0800

    update

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+  );
+}
+
+export default App;
