4c05595c9e21a539cd01e3ee302953eb49272b01
ohaeri ezinne <ohaeriezinne7@gmail.com>
2025-07-22 20:33:24 +0800

    Add inline styling to Header and other components

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
 
 function App() {
