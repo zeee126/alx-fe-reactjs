c6149f6f8e254fcfa984effb67c80e957cdf6532
ohaeri ezinne <ohaeriezinne7@gmail.com>
2025-07-22 20:58:11 +0800

    Initial setup with Zustand store and recipe components

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -3,9 +3,7 @@ import { create } from "zustand";
 const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, newRecipe],
-    })),
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
 }));
 
