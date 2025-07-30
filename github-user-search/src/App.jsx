037cf05bd8df989aaae845fef512a77b064b94b9
ohaeri ezinne <ohaeriezinne7@gmail.com>
2025-07-30 20:13:21 +0800

    github api

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>GitHub User Search</h1>
+      {/* We'll add the Search component and user info here later */}
+    </div>
+  );
+}
+
+export default App;
