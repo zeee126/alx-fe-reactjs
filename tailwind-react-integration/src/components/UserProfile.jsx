2665936b3396ab7eac899781270d4772739ecc7b
ohaeri ezinne <ohaeriezinne7@gmail.com>
2025-08-05 19:55:26 +0800

    tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,15 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-10 md:my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center">
+      <h1 className="sm:text-lg md:text-xl text-xl text-blue-800 my-4">
         John Doe
       </h1>
-      <p className="text-sm md:text-base text-gray-600 text-center">
+      <p className="sm:text-sm md:text-base  text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
